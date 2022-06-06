1bb9ecc7ee4df87ecb15bf02a1b5f3fcba09414f
nandohidayat <mnandohidayat@gmail.com>
2022-06-06 16:05:16 +0700

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Space Grotesk", "sans-serif"],
+      },
+      colors: {
+        gray: {
+          900: "#0F1112",
+          800: "#151718",
+          700: "#181A1E",
+          600: "#55565C",
+        },
+      },
+    },
   },
   plugins: [],
 };
